fb4aea5b79f5d7b3f9acde0e23ac6e31821b2d7f
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2019-11-15 21:11:39 +0600

    initial commit

---
+++ app/src/main/java/com/farhantanvir/graph3d/MainActivity.java
@@ -0,0 +1,39 @@
+package com.farhantanvir.graph3d;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.graph_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.about)
+        {
+            //Toast.makeText(this,"hello world",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
